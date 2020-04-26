55892ed884c3eabaeee810c4de0c0906a9f9316c
Pedro Ribeiro <pmiguelribeiro@ipvc.pt>
2020-04-27 00:44:49 +0100

    Implementação do Login, erro a carregar imagem

--- IntelliCity_ReactProject/App.js
+++ IntelliCity_ReactProject/App.js
@@ -1,67 +1,98 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
-    StyleSheet,
-    View,
-    Text
-  } from 'react-native';
-  
-  import {
-    Colors
-  } from 'react-native/Libraries/NewAppScreen';
-//Novo layout
-  function App(){
-      return(
-          <View style={styles.full}>
-            <View style={styles.half1}>
-                <Text style={styles.text}> This is 1</Text>
-            </View>
-            <View style={styles.half2}>
-                <View style={styles.half21}>
-                    <Text style={styles.text}> This is 2/3</Text>
-                </View>
-                <View style={styles.half22}>
-                    <Text style={styles.text}> This is 1/3</Text>
-                </View>
-            </View>
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Button,
+  TextInput,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+/*
+        <Image
+            style={styles.stretch}
+            source={require('./IntelliCity_ReactProject/Images/react.png')}
+          />
+*/
+function App(){
+    return (
+      <View style={styles.full}>
+        <View style={styles.part1}>
+        <Image
+            style={styles.stretch}
+          />
+        </View>
+        <View style={styles.part2}>
+          <TextInput
+            style={styles.textinput}
+            placeholder="Username"
+          />
+          <TextInput
+            style={styles.textinput}
+            placeholder="Password"
+          />
+          <View style={styles.buttonview}>
+            <Button
+              color="blue"
+              title="Login"
+            />
           </View>
-      );
-  }
+        </View>
+        <View style={styles.part3}>
+            <Button
+              color="orange"
+              title="Acesso anonimo"
+            />
+        </View>
+      </View>
+  );
+}
 
-  const styles = StyleSheet.create({
-    full: {
-        flex: 1,
-    },
-    halfx: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    half1: {
-        flex: 1,
-        backgroundColor: 'red',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    half2: {
-        flex: 1,
-        flexDirection: 'row',
-        backgroundColor: 'blue',
-    },
-    half21:{
-        flex: 2,
-        backgroundColor: 'green',
-        justifyContent: 'center',
-    },
-    half22:{
-        flex: 1,
-        backgroundColor: 'yellow',
-        justifyContent: 'center',       // alinhar vertical
-        alignItems: 'center',           // alinhar horizontal
-    },
-    text:{
-        color: 'black',
-        fontSize: 25,
-    },
-  });
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  part1: {
+    flex: 1,
+//    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  part2: {
+    flex: 2,
+  //  backgroundColor: 'blue',
+  },
+  part3: {
+    flex: 1,
+  //  backgroundColor: 'red',
+    justifyContent: 'flex-end',
+    margin: 10,
+  },
+  buttonview: {
+    flex: 1,
+    margin: 10,
+  },
+  text: {
+    color: 'black',
+    fontSize: 25,
+  },
+  textinput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+  },
+  stretch: {
+    flex: 1,
+    width: 150,
+    height: 150,
+},
+});
 
-  export default App;
+
+export default App;
