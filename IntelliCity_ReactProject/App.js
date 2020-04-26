7a0f7c21bf5b5bb82df69e59595e4f56aade720d
Pedro Ribeiro <pmiguelribeiro@ipvc.pt>
2020-04-26 22:54:37 +0100

    teste 4

--- IntelliCity_ReactProject/App.js
+++ IntelliCity_ReactProject/App.js
@@ -8,7 +8,7 @@ import {
   import {
     Colors
   } from 'react-native/Libraries/NewAppScreen';
-//Novo layout
+//Novo layoute
   function App(){
       return(
           <View style={styles.body}>
